48312e58610f76a9d0b5da6428a50d4d515dbd4d
Angel Marin <angelfloresps08@gmail.com>
2022-05-19 15:35:42 -0700

    Repo Actualizado

---
+++ 02-Intro_a_JS/Carpeta_de_Prueba/js/app.js
@@ -0,0 +1,15 @@
+// let dog = prompt('cual es el significado de la palabra "dog"?');
+//     if(dog.toLowerCase() === 'perro'){
+//         alert('felicidades, has aprendido la palabra "perro" en ingles');
+//     }
+//     else{
+//         alert('felicidades, eres un pendejo');
+//     }
+
+//VACUNACION
+let edad
+let sexo
+let ubiNorte
+let embarazo
+let tiempoEmbarazo
+
---
+++ 02-Intro_a_JS/Proyecto-Cajero_Automatico/js/app.js
@@ -0,0 +1,65 @@
+function accesoCuenta1(form){
+    if (form.u.value=="Mali") {
+        if (form.c.value=="1234") {
+            location="Cuentas/cuenta1.html"
+        } else {
+            alert("error de password");
+        }
+    } else {
+        alert("error de usuario");
+    }
+}
+
+function accesoCuenta2(form){
+    if (form.u.value=="Gera") {
+        if (form.c.value=="4321") {
+            location="Cuentas/cuenta2.html"
+        } else {
+            alert("error de password");
+        }
+    } else {
+        alert("error de usuario");
+    }
+}
+
+function accesoCuenta3(form){
+    if (form.u.value=="Maui") {
+        if (form.c.value=="2468") {
+            location="Cuentas/cuenta3.html"
+        } else {
+            alert("error de password");
+        }
+    } else {
+        alert("error de usuario");
+    }
+}
+
+//FUNCION DEPOSITO
+function Deposito(){
+    //VARIABLES DE LOS SALDOS
+    let Saldo = parseInt(document.getElementById('S').value);
+    let CantidadTotal = parseInt(document.getElementById('Monto').value);
+    
+    //OPERACIONES
+    if(Saldo<990){
+        document.getElementById('S').value =(Saldo+CantidadTotal); 
+    }
+    if((Saldo+CantidadTotal)>990){
+        alert('ADVERTENCIA: La cantidad que desea ingresar supera el limite de saldo en la cuenta');
+    }
+    }
+
+    //FUNCION RETIRO
+    function Retiro(){
+        //VARIABLES DE LOS SALDOS
+        let Saldo = parseInt(document.getElementById('S').value);
+        let CantidadTotal = parseInt(document.getElementById('Monto').value);
+        
+        //OPERACIONES
+        if(Saldo>10){
+            document.getElementById('S').value =(Saldo-CantidadTotal); 
+        }
+        if((Saldo-CantidadTotal)<10){
+        alert('ADVERTENCIA: No puede retirar esa cantidad');
+        }
+        }
